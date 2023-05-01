0c62665af64224ca2993e1e7c53084897d3fddc1
hs122 <hs512128@gmail.com>
2023-05-01 09:48:57 +0530

    Search

--- admin_data/src/App.js
+++ admin_data/src/App.js
@@ -1,9 +1,16 @@
-import Dashboard from './component/Dashboard'
+import Dashboard from './component/Dashboard/Dashboard'
+import Pagination from './component/Pagination/Pagination';
 
 function App() {
   return (
    <>
+   <main>
+   <div class="wrapper">
    <Dashboard/>
+   <Pagination/>
+   </div>
+   </main>
+
    </>
   );
 }
---
+++ admin_data/src/component/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Search } from '../UI/Search/Search'
+
+const Dashboard = () => {
+  return (
+  <table>
+    <tr>
+      <Search/>
+    </tr>
+  </table>
+  )
+}
+
+export default Dashboard
